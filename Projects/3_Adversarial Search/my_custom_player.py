e15e89d0560c041031d0f46d0ab5ca8900cd4823
Seung Jae (Ryan) Lee <seungjaeryanlee@gmail.com>
2018-05-16 17:02:29 +0900

    Completed Project 3 (#3)
    * Implemented CustomPlayer with Minimax and iterative deepening
    
    * Improved first move by choosing position with 8 moves
    
    * Added matches.log to gitignore
    
    * Changed name from minimax to alphabeta
    
    * Added report template
    
    * Added data to report
    
    * Separated failed attempts to improve player
    
    * Stop using 8L for starting moves
    
    * Added negamax implementation
    
    * Implemented negamax with alphabeta
    
    * Organized report
    
    * Updated report about using negamax
    
    * Added square score table for score()
    
    * Disabled endgame cutoff
    
    * Added data for SQ table on report
    
    * Answered questions in report
    
    * Organized code for submission
    
    * Exported report to PDF
    
    * Moved 'import random' to get_action()
    
    * Updated gitignore to ignore udacity submit result

--- Projects/3_Adversarial Search/my_custom_player.py
+++ Projects/3_Adversarial Search/my_custom_player.py
@@ -1,6 +1,6 @@
 
 from sample_players import DataPlayer
-
+import random
 
 class CustomPlayer(DataPlayer):
     """ Implement your own agent to play knight's Isolation
@@ -22,6 +22,20 @@ class CustomPlayer(DataPlayer):
       suitable for using any other machine learning techniques.
     **********************************************************************
     """
+    # Square table for evaluation function
+    SENTINEL = float("-inf")
+    SQ_SCORE = [
+        -3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -3, SENTINEL, SENTINEL,
+        -1,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0, .5, .5, .5, .5, .5,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0, .5,  1,  1,  1, .5,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0, .5,  1,  2,  1, .5,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0, .5,  1,  1,  1, .5,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0, .5, .5, .5, .5, .5,  0,  0, -1, SENTINEL, SENTINEL,
+        -1,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1, SENTINEL, SENTINEL,
+        -3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -3, SENTINEL, SENTINEL,
+    ]
+
     def get_action(self, state):
         """ Employ an adversarial search technique to choose an action
         available in the current state calls self.queue.put(ACTION) at least
@@ -41,9 +55,103 @@ class CustomPlayer(DataPlayer):
         """
         # TODO: Replace the example implementation below with your own search
         #       method by combining techniques from lecture
-        #
-        # EXAMPLE: choose a random move without any search--this function MUST
-        #          call self.queue.put(ACTION) at least once before time expires
-        #          (the timer is automatically managed for you)
+
         import random
-        self.queue.put(random.choice(state.actions()))
+        if state.ply_count < 2: self.queue.put(random.choice(state.actions()))
+        else:
+            # Default: Depth 3
+            # self.queue.put(self.minimax(state, depth=3))
+
+            # Iterative Deepening
+            for i in range(3, 32):
+                # self.queue.put(self.minimax(state, depth=i))
+                self.queue.put(self.alphabeta(state, depth=i))
+                # self.queue.put(self.negamax_root(state, depth=i, color=1))
+                # self.queue.put(self.negamax_alphabeta(state, depth=i, color=1))
+
+    def minimax(self, state, depth):
+        """
+        Minimax search from sample_players.py.
+        """
+        def min_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1))
+            return value
+
+        def max_value(state, depth):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1))
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1))
+
+    def alphabeta(self, state, depth):
+        """
+        Minimax search with alpha-beta pruning.
+        """
+        def min_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("inf")
+            for action in state.actions():
+                value = min(value, max_value(state.result(action), depth - 1, alpha, beta))
+                beta = min(beta, value)
+                if beta <= alpha: break
+            return value
+
+        def max_value(state, depth, alpha, beta):
+            if state.terminal_test(): return state.utility(self.player_id)
+            if depth <= 0: return self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, min_value(state.result(action), depth - 1, alpha, beta))
+                alpha = max(alpha, value)
+                if beta <= alpha: break
+            return value
+
+        return max(state.actions(), key=lambda x: min_value(state.result(x), depth - 1, float("-inf"), float("inf")))
+
+    def negamax_root(self, state, depth, color):
+        """
+        Negamax variant of Minimax.
+        """
+        def negamax(state, depth, color):
+            if state.terminal_test(): return color * state.utility(self.player_id)
+            if depth <= 0: return color * self.score(state)
+            value = float("-inf")
+            for action in state.actions():
+                value = max(value, -negamax(state.result(action), depth - 1, -color))
+            return value
+
+        return max(state.actions(), key=lambda x: -negamax(state.result(x), depth - 1, -color))
+
+    def negamax_alphabeta(self, state, depth, color):
+        """
+        Negamax variant of Minimax with alpha-beta pruning.
+        """
+        def negamax(state, depth, alpha, beta, color):
+            if state.terminal_test(): return color * state.utility(self.player_id)
+            if depth <= 0: return color * self.score(state)
+            best_value = float("-inf")
+            for action in state.actions():
+                value = -negamax(state.result(action), depth - 1, -beta, -alpha, -color)
+                best_value = max(best_value, value)
+                alpha = max(alpha, value)
+                if beta <= alpha: break
+            return best_value
+
+        return max(state.actions(), key=lambda x: -negamax(state.result(x), depth - 1, float("-inf"), float("inf"), -color))
+
+    def score(self, state):
+        own_loc = state.locs[self.player_id]
+        opp_loc = state.locs[1 - self.player_id]
+        own_liberties = state.liberties(own_loc)
+        opp_liberties = state.liberties(opp_loc)
+        
+        return len(own_liberties) - len(opp_liberties) + self.SQ_SCORE[own_loc] - self.SQ_SCORE[opp_loc]
